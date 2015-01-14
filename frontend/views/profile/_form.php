ccb882a32f2f10cd4408c16230863ca83084705f
ilya3d <ilya@twinscom.ru>
2015-01-14 11:42:24 +0300

    edit user profile - ext fields list

--- frontend/views/profile/_form.php
+++ frontend/views/profile/_form.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\jui\DatePicker;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
@@ -12,6 +13,8 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
+    <?= $form->field($model, 'username')->textInput(['maxlength' => 255]) ?>
+
     <?= $form->field($model, 'phone')->textInput(['maxlength' => 255]) ?>
 
     <?= $form->field($model, 'skype')->textInput(['maxlength' => 255]) ?>
@@ -19,6 +22,17 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'icq')->textInput(['maxlength' => 255]) ?>
 
     <?= $form->field($model, 'address')->textInput(['maxlength' => 255]) ?>
+
+    <?= $form->field($model,'birthday')->widget(DatePicker::className(),[
+        'clientOptions' => [
+            'defaultDate' => '2014-01-01',
+            'changeYear'=>true,
+            'changeMonth'=>true,
+            'yearRange'=> '1960:2000'
+        ],
+        'language' => 'ru',
+        'dateFormat' => 'dd.MM.yyyy'
+    ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
