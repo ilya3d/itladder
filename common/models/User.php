e51a2c40bdc3d01f45c4489d7163edcf2df3f3ca
Maxim Kuptsov <maximkuptsov@gmail.com>
2015-01-15 14:03:20 +0300

    check hash_sum

--- common/models/User.php
+++ common/models/User.php
@@ -302,6 +302,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public function validatePassword($password)
     {
+        if (!$this->password_hash) return false;
         return Yii::$app->security->validatePassword($password, $this->password_hash);
     }
 
